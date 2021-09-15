125dfa1805edb727a2b11e9f731a8fc8383aa2ab
1JakeAdams <90648121+1JakeAdams@users.noreply.github.com>
2021-09-15 14:44:22 -0500

    Added Code

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,6 @@
+let petnames = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge","Sam"]
+let withSal = petnames
+withSal.push("Sal")
+console.log(`The Third pet is named: ${petnames[2]}`)
+console.log(`The array has ${withSal.length} names in it.`)
+withSal.pop()
